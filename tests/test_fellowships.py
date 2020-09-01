db3e4ad63dae5606b0933a8807b7dec0a1c3fa64
TLESORT <t.lesort@gmail.com>
2020-09-01 11:13:42 +0200

    test fellowship with manually created settings

--- tests/test_fellowships.py
+++ tests/test_fellowships.py
@@ -9,12 +9,12 @@ from continuum import ClassIncremental
 
 @pytest.mark.slow
 def test_MNIST_Fellowship():
-    cl_dataset = MNISTFellowship(data_path="./pytest/Samples/Datasets", train=True, download=True)
+    cl_dataset = MNISTFellowship(data_path="./tests/Datasets", train=True, download=True)
 
 
 @pytest.mark.slow
 def test_CIFAR_Fellowship():
-    cl_dataset = CIFARFellowship(data_path="./pytest/Samples/Datasets", train=True, download=True)
+    cl_dataset = CIFARFellowship(data_path="./tests/Datasets", train=True, download=True)
 
 
 
@@ -24,16 +24,29 @@ def test_CIFAR_Fellowship():
                                            [CIFAR10, CIFAR100],
                                            [KMNIST, MNIST, FashionMNIST, CIFAR10, CIFAR100]])
 def test_Fellowship(list_datasets):
-    cl_dataset = Fellowship(data_path="./pytest/Samples/Datasets", dataset_list=list_datasets)
+    cl_dataset = Fellowship(data_path="./tests/Datasets", dataset_list=list_datasets)
 
 
 @pytest.mark.slow
 @pytest.mark.parametrize("list_datasets", [[MNIST, FashionMNIST],
-                                           [KMNIST, MNIST, FashionMNIST],
-                                           [KMNIST, MNIST, FashionMNIST, CIFAR10]])
+                                           [KMNIST, MNIST, FashionMNIST]])
+#@pytest.mark.parametrize("shared_label_space", [True, False])
 def test_Fellowship_classes(list_datasets):
-    cl_dataset = Fellowship(data_path="./pytest/Samples/Datasets", dataset_list=list_datasets)
+    cl_dataset = Fellowship(data_path="./tests/Datasets", dataset_list=list_datasets)
     continuum = ClassIncremental(cl_dataset, increment=10)
 
     for task_id, taskset in enumerate(continuum):
-        taskset
+
+        classes = taskset.get_classes()
+
+        # we check if all classes are here
+        assert len(classes) == (classes.max()-classes.min()+1)
+
+@pytest.mark.slow
+@pytest.mark.parametrize("list_datasets", [[MNIST, CIFAR10]])
+def test_Fellowship_Dimension_Fail(list_datasets):
+    cl_dataset = Fellowship(data_path="./tests/Datasets", dataset_list=list_datasets)
+
+    # This does not work since CIFAR10 and MNIST data are not same shape
+    with pytest.raises(ValueError):
+        continuum = ClassIncremental(cl_dataset, increment=10)
