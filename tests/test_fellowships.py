c8d82db957e1ee8249b2b4359f27e5674970b5b1
TLESORT <t.lesort@gmail.com>
2020-09-01 09:51:13 +0200

    test fellowship with various association of datasets

--- tests/test_fellowships.py
+++ tests/test_fellowships.py
@@ -2,8 +2,10 @@ import numpy as np
 import pytest
 import os
 
-from continuum.datasets import MNIST
-from continuum.datasets import CIFARFellowship, MNISTFellowship
+from continuum.datasets import MNIST, CIFAR10, CIFAR100, KMNIST, FashionMNIST
+from continuum.datasets import Fellowship, CIFARFellowship, MNISTFellowship
+
+from continuum import ClassIncremental
 
 @pytest.mark.slow
 def test_MNIST_Fellowship():
@@ -13,3 +15,25 @@ def test_MNIST_Fellowship():
 @pytest.mark.slow
 def test_CIFAR_Fellowship():
     cl_dataset = CIFARFellowship(data_path="./pytest/Samples/Datasets", train=True, download=True)
+
+
+
+@pytest.mark.slow
+@pytest.mark.parametrize("list_datasets", [[MNIST, FashionMNIST],
+                                           [KMNIST, MNIST, FashionMNIST],
+                                           [CIFAR10, CIFAR100],
+                                           [KMNIST, MNIST, FashionMNIST, CIFAR10, CIFAR100]])
+def test_Fellowship(list_datasets):
+    cl_dataset = Fellowship(data_path="./pytest/Samples/Datasets", dataset_list=list_datasets)
+
+
+@pytest.mark.slow
+@pytest.mark.parametrize("list_datasets", [[MNIST, FashionMNIST],
+                                           [KMNIST, MNIST, FashionMNIST],
+                                           [KMNIST, MNIST, FashionMNIST, CIFAR10]])
+def test_Fellowship_classes(list_datasets):
+    cl_dataset = Fellowship(data_path="./pytest/Samples/Datasets", dataset_list=list_datasets)
+    continuum = ClassIncremental(cl_dataset, increment=10)
+
+    for task_id, taskset in enumerate(continuum):
+        taskset
