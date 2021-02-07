3ae1ee4d08b8fd3bf072a09675019113640ff1cb
TLESORT <t.lesort@gmail.com>
2021-02-07 11:48:25 -0500

    save dict function added

--- continuum/metrics/dev_logger.py
+++ continuum/metrics/dev_logger.py
@@ -1,5 +1,6 @@
 import abc
 import collections
+import os
 import torch
 import numpy as np
 from continuum.metrics.base_logger import _BaseLogger
@@ -7,32 +8,25 @@ from continuum.metrics.metrics import get_model_size
 
 
 class Dev_Logger(_BaseLogger):
-    def __init__(self, root_log=".", list_keywords=["performance"], list_fields=["train", "eval"]):
+    def __init__(self, root_log=None, list_keywords=["performance"], list_subsets=["train", "eval"]):
+        self.root_log = root_log
         self.list_keywords = list_keywords
-        self.list_fields = list_fields
+        self.list_subsets = list_subsets
 
         assert self.list_keywords is not None
         assert self.list_fields is not None
         assert len(self.list_keywords) >= 1
         assert len(self.list_subsets) >= 1
 
-        logger_dic = {}
+        self.logger_dict = {}
 
         for keyword in self.list_keywords:
-            logger_dic[keyword] = {}
+            self.logger_dict[keyword] = {}
             for subset in self.list_subsets:
-                logger_dic[keyword][subset] = {}
+                self.logger_dict[keyword][subset] = {}
 
         self.current_task = 0
         self.current_epoch = 0
-
-        self._predictions = collections.defaultdict(list)
-        self._targets = collections.defaultdict(list)
-        self._tasks = collections.defaultdict(list)
-        self._model_sizes = []
-
-        self._batch_predictions = []
-        self._batch_targets = []
 
     def add(self, value, keyword, subset):
 
@@ -61,25 +55,37 @@ class Dev_Logger(_BaseLogger):
         if not isinstance(targets, np.ndarray):
             raise TypeError(f"Provide targets as np.array, not {type(predictions).__name__}.")
 
-        self.logger_dic["performance"][subset][self.current_task][self.current_epoch]["predictions"].append(predictions)
-        self.logger_dic["performance"][subset][self.current_task][self.current_epoch]["targets"].append(targets)
-        self.logger_dic["performance"][subset][self.current_task][self.current_epoch]["task_ids"].append(task_ids)
+        self.logger_dict["performance"][subset][self.current_task][self.current_epoch]["predictions"].append(predictions)
+        self.logger_dict["performance"][subset][self.current_task][self.current_epoch]["targets"].append(targets)
+        self.logger_dict["performance"][subset][self.current_task][self.current_epoch]["task_ids"].append(task_ids)
+
+    def update_dict_architecture(self, update_task=False):
+        for keyword in self.list_keywords:
+            if update_task:
+                self.logger_dict[keyword][subset][self.current_task] = {}
+            for subset in self.list_subsets:
+                self.logger_dict[keyword][subset][self.current_task][self.current_epoch] = {}
 
     def new_epoch(self):
         self.current_epoch += 1
+        self.update_dict_architecture(update_task=False)
 
     def new_task(self):
-        self.save_dic()
+        if self.root_log is not None:
+            self.save_dic()
         self.current_task += 1
-        self.epoch = 0
+        self.current_epoch = 0
+        self.update_dict_architecture(update_task=True)
+
 
     def save_dic(self):
         import pickle as pkl
         filename = f"logger_dic_task_{self.current_task}.pkl"
+        filename = os.path.join(self.root_log, filename)
         with open(filename, 'wb') as f:
-            pkl.dump(self.logger_dic, f, pkl.HIGHEST_PROTOCOL)
+            pkl.dump(self.logger_dict, f, pkl.HIGHEST_PROTOCOL)
 
         # after saving values we remove them from dictionnary to save space and memory
         for keyword in self.list_keywords:
             for subset in self.list_subsets:
-                self.logger_dic["performance"][subset][self.current_task] = None
+                self.logger_dict[keyword][subset][self.current_task] = None
