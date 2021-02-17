d4ac594bf2f04e9d98dea783368c2d313c85c48e
arthurdouillard <ar.douillard@gmail.com>
2021-02-17 11:59:25 +0100

    Remove unused DataIncremental.
    We may re-add it later if we find the need.

--- continuum/scenarios/__init__.py
+++ continuum/scenarios/__init__.py
@@ -2,14 +2,12 @@
 # flake8: noqa
 from continuum.scenarios.base import _BaseScenario
 from continuum.scenarios.class_incremental import ClassIncremental
-from continuum.scenarios.data_incremental import DataIncremental
 from continuum.scenarios.instance_incremental import InstanceIncremental
 from continuum.scenarios.transformation_incremental import TransformationIncremental
 from continuum.scenarios.rotations import Rotations
 from continuum.scenarios.permutations import Permutations
 
 __all__ = ["ClassIncremental",
-           "DataIncremental",
            "InstanceIncremental",
            "Rotations",
            "Permutations",
