577ca42d788c9361e40c4895ffb5d34f54bd2ad0
arthurdouillard <ar.douillard@gmail.com>
2021-02-26 13:34:30 +0100

    Add Segmentation (disjoint+overlap) scenario.

--- continuum/scenarios/segmentation.py
+++ continuum/scenarios/segmentation.py
@@ -1,14 +1,17 @@
 import warnings
 from copy import copy
-from typing import Callable, List, Union
+from typing import Callable, List, Union, Optional
 
 import numpy as np
+from PIL import Image
+import torchvision
 
 from continuum.datasets import _ContinuumDataset
-from continuum.scenarios import _BaseScenario
+from continuum.scenarios import ClassIncremental
+from continuum.tasks import TaskSet
 
 
-class SegmentationClassIncremental(_BaseScenario):
+class SegmentationClassIncremental(ClassIncremental):
     """Continual Loader, generating datasets for the consecutive tasks.
 
     Scenario: Each new tasks bring new classes only
@@ -28,140 +31,122 @@ class SegmentationClassIncremental(_BaseScenario):
     def __init__(
         self,
         cl_dataset: _ContinuumDataset,
-        nb_tasks: int = 0,
+        nb_classes: int = 0,
         increment: Union[List[int], int] = 0,
         initial_increment: int = 0,
         transformations: List[Callable] = None,
-        class_order: Union[List[int], None]=None,
+        class_order: Optional[List[int]]=None,
         mode: str = "overlap",
+        save_indexes: Optional[str] = None,
         test_background: bool = True
     ) -> None:
+        self.mode = mode
+        self.save_indexes = save_indexes
+        self.test_background = test_background
+        self._nb_classes = nb_classes
+
         super().__init__(
             cl_dataset=cl_dataset,
-            nb_tasks=nb_tasks,
             increment=increment,
             initial_increment=initial_increment,
             class_order=class_order,
             transformations=transformations,
         )
 
+    @property
+    def nb_classes(self) -> int:
+        """Total number of classes in the whole continual setting."""
+        return len(np.unique(self.dataset[1]))  # type: ignore
+
+    def __getitem__(self, task_index: Union[int, slice]):
+        """Returns a task by its unique index.
+
+        :param task_index: The unique index of a task. As for List, you can use
+                           indexing between [0, len], negative indexing, or
+                           even slices.
+        :return: A train PyTorch's Datasets.
+        """
+        if isinstance(task_index, slice) and task_index.step is not None:
+            raise ValueError("Step in slice for segmentation is not supported.")
+
+        x, y, t, task_index = self._select_data_by_task(task_index)
+
+        if self.mode in ("overlap", "disjoint"):
+            labels = self._get_task_labels(task_index)
+
+            inverted_order = {label: self.class_order.index(label) for label in labels}
+            if not self.cl_dataset.train:
+                inverted_order[0] = 0 if self.test_background else 255
+                inverted_order[255] = 255
+
+            label_trsf = torchvision.transforms.Lambda(
+                lambda seg_map: seg_map.apply_(
+                    lambda v: inverted_order.get(v, 0)
+                )
+            )
+
+        return TaskSet(x, y, t, self.trsf, target_trsf=label_trsf, data_type=self.cl_dataset.data_type)
+
+    def _get_task_labels(self, task_indexes: Union[int, List[int]]) -> List[int]:
+        if isinstance(task_indexes, int):
+            task_indexes = [task_indexes]
+
+        labels = set()
+        for t in task_indexes:
+           previous_inc = sum(self._increments[:t])
+           labels.update(
+               self.class_order[previous_inc:previous_inc+self._increments[t]]
+           )
+
+        return list(labels)
+
     def _setup(self, nb_tasks: int) -> int:
-
         x, y, _ = self.cl_dataset.get_data()
-        unique_classes = np.unique(y)
+        self.class_order = list(range(1, self._nb_classes + 1))
 
-        self.class_order = self.class_order or self.cl_dataset.class_order or list(
-            range(len(unique_classes))
+        self._increments = self._define_increments(
+            self.increment, self.initial_increment, self.class_order
         )
 
-        if len(np.unique(self.class_order)) != len(self.class_order):
-            raise ValueError(f"Invalid class order, duplicates found: {self.class_order}.")
+        t = np.ones(len(x)) * -1
+        accumulated_inc = 0
+        t = _filter_images(
+            y, self._increments, self.class_order, self.mode
+        )
+        self.dataset = (x, y, t)
 
-        new_y = np.vectorize(self.class_order.index)(y)
+        return len(self._increments)
 
-        # Increments setup
-        self.class_order = np.array(self.class_order)
-        if nb_tasks <= 0:
-            # The number of tasks is left unspecified, thus it will be determined
-            # by the specified increments.
-            self.increments = self._define_increments(
-                self.increment, self.initial_increment, unique_classes
-            )
-        else:
-            # A fixed number of tasks is required, thus the all increments will
-            # be equal among tasks.
-            if self.increment > 0:
-                warnings.warn(
-                    f"When both `nb_tasks` (given value = {nb_tasks}) and "
-                    f"`increment` (given value = {self.increment} are both set, "
-                    "we only consider the number of tasks. The `increment` "
-                    "argument is ignored."
-                )
-            increment = len(unique_classes) / nb_tasks
-            if not increment.is_integer():
-                raise Exception(
-                    f"Invalid number of tasks ({nb_tasks}) for {len(unique_classes)} classes."
-                )
-            self.increments = [int(increment) for _ in range(nb_tasks)]
 
-        # compute task label
-        task_ids = self._set_task_labels(new_y)
+def _filter_images(paths, increments, class_order, mode="overlap"):
+    """Select images corresponding to the labels.
 
-        # Dataset with task label
-        self.dataset = (x, new_y, task_ids)  # (data, class label, task label)
+    Strongly inspired from Cermelli's code:
+    https://github.com/fcdl94/MiB/blob/master/dataset/utils.py#L19
+    """
+    indexes_to_classes = []
+    for path in paths:
+        classes = np.unique(np.array(Image.open(path)).reshape(-1))
+        indexes_to_classes.append(classes)
 
-        return len(np.unique(task_ids))
+    t = np.zeros((len(paths), len(increments)))
+    accumulated_inc = 0
 
-    def _set_task_labels(self, y: np.ndarray) -> np.ndarray:
-        """For each data point, defines a task associated with the data.
+    for task_id, inc in enumerate(increments):
+        labels = class_order[accumulated_inc:accumulated_inc+inc]
+        old_labels = class_order[:accumulated_inc]
+        all_labels = labels + old_labels + [0, 255]
 
-        :param y: label tensor
-        :param increments: increments contains information about classes per tasks
-        :return: tensor of task label
-        """
-        t = copy(y)  # task label as same size as y
+        for index, classes in enumerate(indexes_to_classes):
+            if mode == "overlap":
+                if any(c in labels for c in classes):
+                    t[index, task_id] = 1
+            elif mode == "disjoint":
+                if any(c in labels for c in classes) and all(c in all_labels for c in classes):
+                    t[index, task_id] = 1
+            else:
+                raise ValueError(f"Unknown mode={mode}.")
 
-        for task_index, _ in enumerate(self.increments):
-            max_class = sum(self.increments[:task_index + 1])
-            min_class = sum(self.increments[:task_index])  # 0 when task_index == 0.
+        accumulated_inc += inc
 
-            indexes = np.where(np.logical_and(y >= min_class, y < max_class))[0]
-            t[indexes] = task_index
-        return t
-
-    def _define_increments(
-        self, increment: Union[List[int], int], initial_increment: int, unique_classes: List[int]
-    ) -> List[int]:
-
-        if isinstance(increment, list):
-            # Check if the total number of classes is compatible
-            # with increment list and self.nb_classes
-            if not sum(increment) == len(unique_classes):
-                raise Exception("The increment list is not compatible with the number of classes")
-
-            increments = increment
-        elif isinstance(increment, int) and increment > 0:
-            increments = []
-            if initial_increment:
-                increments.append(initial_increment)
-
-            nb_tasks = (len(unique_classes) - initial_increment) / increment
-            if not nb_tasks.is_integer():
-                raise Exception(
-                    "The tasks won't have an equal number of classes"
-                    f" with {len(self.class_order)} and increment {increment}"
-                )
-            increments.extend([increment for _ in range(int(nb_tasks))])
-        else:
-            raise TypeError(f"Invalid increment={increment}, it must be an int > 0.")
-
-        return increments
-
-    def get_original_targets(self, targets: np.ndarray) -> np.ndarray:
-        """Returns the original targets not changed by the custom class order.
-
-        :param targets: An array of targets, as provided by the task datasets.
-        :return: An array of targets, with their original values.
-        """
-        return self.class_order[targets]
-
-    def _select_data_by_classes(self, min_class_id: int, max_class_id: int):
-        """Selects a subset of the whole data for a given set of classes.
-
-        :param min_class_id: The minimum class id.
-        :param max_class_id: The maximum class id.
-        :return: A tuple of numpy array, the first item being the data and the
-                 second the associated targets.
-        """
-        x_, y_, _ = self.dataset
-
-        indexes = np.where(np.logical_and(y_ >= min_class_id, y_ < max_class_id))[0]
-        selected_x = x_[indexes]
-        selected_y = y_[indexes]
-
-        if self.cl_dataset.need_class_remapping:
-            # A remapping of the class ids is done to handle some special cases
-            # like PermutedMNIST or RotatedMNIST.
-            selected_y = self.cl_dataset.class_remapping(selected_y)
-
-        return selected_x, selected_y
+    return t
