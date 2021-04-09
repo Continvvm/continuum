16deee978e195869ef7e3803d3f0dc45cf5ae68c
arthurdouillard <ar.douillard@gmail.com>
2021-04-09 15:45:04 +0200

    Bump version to 1.0.14

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="continuum",
-    version="1.0.12",
+    version="1.0.14",
     author="Arthur Douillard, Timothée Lesort",
     author_email="ar.douillard@gmail.com",
     description="A clean and simple library for Continual Learning in PyTorch.",
