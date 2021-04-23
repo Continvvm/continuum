57ad067f1bf940bd68e6e57e061049ce34041260
arthurdouillard <ar.douillard@gmail.com>
2021-04-23 12:06:38 +0200

    Add Rainbow MNIST (only color for now).

--- continuum/datasets/__init__.py
+++ continuum/datasets/__init__.py
@@ -15,4 +15,4 @@ from continuum.datasets.pascalvoc import PascalVOC2012
 from continuum.datasets.stream51 import Stream51
 from continuum.datasets.dtd import DTD
 from continuum.datasets.ctrl import CTRLplus, CTRLminus, CTRLin, CTRLout, CTRLpl
-
+from continuum.datasets.rainbow_mnist import RainbowMNIST
---
+++ continuum/datasets/rainbow_mnist.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+from continuum.datasets import MNIST
+
+
+
+class RainbowMNIST(MNIST):
+    def __init__(*args, color="red", rotation=0., scale=1., **kwargs):
+        if rotation != 0. or scale != 1.:
+            raise NotImplementedError(
+                "Rotation and scale are not supported yet for RainbowMNIST."
+            )
+        if color not in ("red", "blue", "green"):
+            raise ValueError(
+                f"Only red, blue, and green colors are available, not {color}."
+            )
+
+        super().__init__(*args, **kwargs)
+
+        self.color = color
+        self.rotation = rotation
+        self.scale = scale
+
+    def get_data(self):
+        x, y, t = super().get_data()
+        x = x[..., None]
+
+        if self.color == "red":
+            c = np.zeros((x.shape[0], x.shape[1], x.shape[2], 2), dtype=np.uint8)
+            x = np.concatenate((x, c), axis=-1)
+        elif self.color == "green":
+            c = np.zeros((x.shape[0], x.shape[1], x.shape[2], 1), dtype=np.uint8)
+            x = np.concatenate((c, x, c), axis=-1)
+        else:
+            c = np.zeros((x.shape[0], x.shape[1], x.shape[2], 2), dtype=np.uint8)
+            x = np.concatenate((c, x), axis=-1)
+
+        return x, y, t
