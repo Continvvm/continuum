f59b2ab0cf07e5bf645c7fd8bb115c18b9264249
arthurdouillard <ar.douillard@gmail.com>
2021-08-19 17:52:46 +0200

    Support advanced indexing with transformation incremental.

--- continuum/scenarios/transformation_incremental.py
+++ continuum/scenarios/transformation_incremental.py
@@ -1,4 +1,4 @@
-from typing import Callable, List
+from typing import Callable, List, Optional
 
 import numpy as np
 from torchvision import transforms
@@ -30,15 +30,17 @@ class TransformationIncremental(InstanceIncremental):
             shared_label_space=True
     ):
         nb_tasks = len(incremental_transformations)
-        super().__init__(
-            cl_dataset=cl_dataset, nb_tasks=nb_tasks, transformations=base_transformations
-        )
         if incremental_transformations is None:
             raise ValueError("For this scenario a list transformation should be set")
 
         self.inc_trsf = incremental_transformations
-        self._nb_tasks = self._setup(nb_tasks)
+        #self._nb_tasks = self._setup(nb_tasks)
         self.shared_label_space = shared_label_space
+
+        super().__init__(
+            cl_dataset=cl_dataset, nb_tasks=nb_tasks, transformations=base_transformations
+        )
+
         self.num_classes_per_task = len(np.unique(self.dataset[1]))  # the num of classes is the same for all task is this scenario
 
     @property
@@ -71,19 +73,51 @@ class TransformationIncremental(InstanceIncremental):
         :param task_index: The unique index of a task, between 0 and len(loader) - 1.
         :return: A train PyTorch's Datasets.
         """
-        if isinstance(task_index, slice):
-            raise ValueError(
-                "Incremental training based on transformations "
-                "does not support slice, please provide only integer."
-            )
-        elif task_index < 0:  # Support for negative index, e.g. -1 == last
-            while task_index < 0:
-                task_index += len(self)
+        x, y, _ = self.dataset
 
-        self.update_task_indexes(task_index)
-        if not self.shared_label_space:
-            self.update_labels(task_index)
-        x, y, t, _ = self._select_data_by_task(task_index)
-        trsf = self.get_task_transformation(task_index)
+        if isinstance(task_index, slice):
+            start = task_index.start if task_index.start is not None else 0
+            stop = task_index.stop if task_index.stop is not None else len(self) + 1
+            step = task_index.step if task_index.step is not None else 1
+            task_index = list(range(start, stop, step))
+            if len(task_index) == 0:
+                raise ValueError(f"Invalid slicing resulting in no data (start={start}, end={stop}, step={step}).")
+        elif isinstance(task_index, np.ndarray):
+            task_index = list(task_index)
+        elif isinstance(task_index, int):
+            task_index = [task_index]
+        else:
+            raise TypeError(f"Invalid type of task index {type(task_index).__name__}.")
+
+        task_index = set([_handle_negative_indexes(ti, len(self)) for ti in task_index])
+
+        t = np.concatenate([
+            (np.ones(len(x)) * ti).astype(np.int32) for ti in task_index
+        ])
+        x = np.concatenate([
+            x for _ in range(len(task_index))
+        ])
+        if self.shared_label_space:
+            y = np.concatenate([
+                y for _ in range(len(task_index))
+            ])
+        else:
+            # Different transformations have different labels even though
+            # the original images were the same
+            y = np.concatenate([
+                y + ti * self.num_classes_per_task for ti in task_index
+            ])
+
+        trsf = [  # Non-used tasks have a None trsf
+            self.get_task_transformation(ti)
+            if ti in task_index else None
+            for ti in range(len(self))
+        ]
 
         return TaskSet(x, y, t, trsf, data_type=self.cl_dataset.data_type)
+
+
+def _handle_negative_indexes(index: int, total_len: int) -> int:
+    while index < 0:
+        index += total_len
+    return index
