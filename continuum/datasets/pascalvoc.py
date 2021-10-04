d40bc67cc3784b09486aad265ed7e79060f0f9fc
arthurdouillard <ar.douillard@gmail.com>
2021-10-04 17:36:56 +0200

    Add PascalVOC2007 for classification.

--- continuum/datasets/pascalvoc.py
+++ continuum/datasets/pascalvoc.py
@@ -73,3 +73,85 @@ class PascalVOC2012(_ContinuumDataset):
                 map_paths.append(os.path.join(self.data_path, p2[1:].strip()))
 
         return np.array(image_paths), np.array(map_paths), None
+
+
+class PascalVOC2007(_ContinuumDataset):
+    """Pascal VOC 2007.
+
+    For now, it only supports classification, which is the most commonly used
+    setting with this dataset.
+    If you wish to do segmentation, you should consider usinng PascalVOC2012.
+    """
+    url = "http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar"
+
+
+    def __init__(self, data_path, train: bool = True, download: bool = True, mode: str = "classification"):
+        if mode not in ("classification", "detection", "segmentation"):
+            raise ValueError(f"Unsupported target <{mode}>, available are "
+                             " <classification>, <detection>, and <segmentation>.")
+        if mode in ("detection", "segmentation"):
+            raise NotImplementedError(f"This mode <{mode}> is not yet supported.")
+
+        self.mode = mode
+
+        super().__init__(data_path, train, download)
+
+    @property
+    def data_type(self) -> TaskType:
+        if self.mode == "classification":
+            return TaskType.IMAGE_PATH
+        elif self.mode  == "segmentation":
+            return TaskType.SEGMENTATION
+        return TaskType.DETECTION
+
+    def _download(self):
+        if not os.path.exists(os.path.join(self.data_path, "VOCdevkit", "VOC2007")):
+            archive_path = os.path.join(self.data_path, "VOCtrainval_06-Nov-2007.tar")
+
+            if not os.path.exists(archive_path):
+                print(f"Downloading archive ...", end=" ")
+                download.download(self.url, self.data_path)
+                print('Done!')
+
+            print(f"Extracting archive...", end=" ")
+            download.untar(archive_path)
+            print("Done!")
+
+    def get_data(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        classes = [
+            "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car",
+            "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike",
+            "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"
+        ]
+
+        if self.train:
+            suffix = "train"
+        else:
+            suffix = "val"
+
+        x, y = [], []
+        for class_id, class_name in enumerate(classes):
+            path = os.path.join(
+                self.data_path,
+                "VOCdevkit", "VOC2007", "ImageSets", "Main",
+                f"{class_name}_{suffix}.txt"
+            )
+
+            with open(path) as f:
+                for line in f:
+                    line = line.split(" ")
+                    presence = line[-1].strip()
+                    if presence != "1":
+                        continue
+
+                    image_id = line[0].strip()
+                    x.append(
+                        os.path.join(
+                            self.data_path,
+                            "VOCdevkit", "VOC2007", "JPEGImages",
+                            f"{image_id}.jpg"
+                        )
+                    )
+                    y.append(class_id)
+
+        return np.array(x), np.array(y), None
