066ca4ba395a370ce32e12ee4d704698878236f4
arthurdouillard <ar.douillard@gmail.com>
2021-10-06 10:42:24 +0200

    Add task type to STL.

--- continuum/datasets/stl10.py
+++ continuum/datasets/stl10.py
@@ -1,12 +1,22 @@
 from typing import List, Tuple, Union
 
 import numpy as np
-from torchvision import datasets as torchdata
-
 from continuum.datasets import _ContinuumDataset
+from continuum.tasks import TaskType
+from torchvision import datasets as torchdata
 
 
 class STL10(_ContinuumDataset):
+    """STL10 Dataset.
+
+    - 10 classes
+    - 500 training images, 800 test images
+    - 96x96x3
+
+    * An Analysis of Single Layer Networks in Unsupervised Feature Learning
+      Adam Coates, Honglak Lee, Andrew Y. Ng
+      AISTATS 2011
+    """
     def __init__(
             self, data_path: str = "", train: Union[bool, str] = True, download: bool = True):
 
@@ -21,6 +31,10 @@ class STL10(_ContinuumDataset):
             download=self.download,
             split=self.train
     )
+
+    @property
+    def data_type(self) -> TaskType:
+        return TaskType.IMAGE_ARRAY
 
     def get_data(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
         x, y = np.array(self.dataset.data), np.array(self.dataset.labels)
