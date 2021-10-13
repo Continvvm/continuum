f7f7a899bdb8baf930c340fce8a90aea88296730
TLESORT <t.lesort@gmail.com>
2021-10-13 15:23:29 -0400

    add an inference function for fancy feature extractions

--- continuum/scenarios/scenario_utils.py
+++ continuum/scenarios/scenario_utils.py
@@ -40,7 +40,19 @@ def create_subscenario(base_scenario, task_indexes):
 
 
 @torch.no_grad()
-def encode_into_dataset(model, scenario, batch_size, filename, train):
+def encode_into_dataset(model, scenario, batch_size, filename, inference_fct=None):
+    """This function encode a scenario into a h5 dataset to reproduce the same scenario with features.
+
+    :param model: model to encode the data.
+    :param scenario: scenario to encode.
+    :param batch_size: batch size to load data.
+    :param filename: filename for the h5 dataset.
+    :param inference_fct: A function that make possible to have a sophisticate way to get features.
+    """
+
+    if inference_fct is None:
+        inference_fct = (lambda model, x: model(x))
+
     # we save feature in eval mode
     model.eval()
 
@@ -49,7 +61,7 @@ def encode_into_dataset(model, scenario, batch_size, filename, train):
         # we need to load the data to use the transformation if there is some
         loader = DataLoader(taskset, shuffle=False, batch_size=batch_size)
         for i, (x, y, t) in enumerate(loader):
-            features = model(x.cuda())
+            features = inference_fct(model,x.cuda())
             if t is None:
                 t = (torch.ones(len(y)) * task_id).long()
 
@@ -61,11 +73,12 @@ def encode_into_dataset(model, scenario, batch_size, filename, train):
     return encoded_dataset
 
 
-def encode_scenario(scenario, model, batch_size, file_name, train=True):
+def encode_scenario(scenario, model, batch_size, file_name, inference_fct=None):
     if os.path.isfile(file_name):
         raise ValueError("File name already exists")
 
-    print(f"Encoding {file_name}")
-    encoded_dataset = encode_into_dataset(model, scenario, batch_size, file_name, train)
+    print(f"Encoding {file_name}.")
+    encoded_dataset = encode_into_dataset(model, scenario, batch_size, file_name, inference_fct)
+    print(f"Encoding is done.")
 
     return ContinualScenario(encoded_dataset)
