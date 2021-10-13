8e35f1108ecfe61870bb0f5a00e030473df6d705
TLESORT <t.lesort@gmail.com>
2021-10-13 15:01:34 -0400

    adapt encode to the new h5Dataset architecture

--- continuum/scenarios/scenario_utils.py
+++ continuum/scenarios/scenario_utils.py
@@ -7,6 +7,7 @@ from continuum.datasets import InMemoryDataset, H5Dataset
 from continuum.scenarios import ContinualScenario
 from continuum.tasks import TaskType
 
+
 def create_subscenario(base_scenario, task_indexes):
     """
     In this function we want to create a subscenario from the different tasks, either by subsampling tasks or reodering
@@ -38,50 +39,33 @@ def create_subscenario(base_scenario, task_indexes):
     return ContinualScenario(dataset, transformations=base_scenario.transformations)
 
 
+@torch.no_grad()
 def encode_into_dataset(model, scenario, batch_size, filename, train):
     # we save feature in eval mode
     model.eval()
 
-    list_features = []
-    list_labels = []
-    list_tasks_labels = []
-    with torch.no_grad():
-        for task_id, taskset in enumerate(scenario):
-            # we need to load the data to use the transformation if there is some
-            loader = DataLoader(taskset, shuffle=False, batch_size=batch_size)
-            for i, (x, y, t) in enumerate(loader):
-                features = model.feature_extractor(x.cuda())
-                list_features.append(features.detach().cpu())
-                list_labels.append(y)
-                if t is None:
-                    t = (torch.ones(len(y)) * task_id).long()
-                list_tasks_labels.append(t)
-
-            # convert into torch tensor
-            feature_vector = torch.cat(list_features).numpy()
-            label_vector = torch.cat(list_labels).numpy()
-            tasks_labels_vector = torch.cat(list_tasks_labels).numpy()
+    encoded_dataset = None
+    for task_id, taskset in enumerate(scenario):
+        # we need to load the data to use the transformation if there is some
+        loader = DataLoader(taskset, shuffle=False, batch_size=batch_size)
+        for i, (x, y, t) in enumerate(loader):
+            features = model(x.cuda())
+            if t is None:
+                t = (torch.ones(len(y)) * task_id).long()
 
             if task_id == 0:
-                encoded_dataset = H5Dataset(x=feature_vector,
-                                            y=label_vector,
-                                            t=tasks_labels_vector,
-                                            data_path=filename,
-                                            data_type=TaskType.TENSOR)
+                encoded_dataset = H5Dataset(features.cpu(), y, t, data_path=filename)
             else:
-                encoded_dataset.add_data(x=feature_vector,
-                                         y=label_vector,
-                                         t=tasks_labels_vector)
+                encoded_dataset.add_data(features.cpu(), y, t)
 
     return encoded_dataset
 
+
 def encode_scenario(scenario, model, batch_size, file_name, train=True):
     if os.path.isfile(file_name):
-        raise AssertionError("File name already exists")
+        raise ValueError("File name already exists")
 
     print(f"Encoding {file_name}")
     encoded_dataset = encode_into_dataset(model, scenario, batch_size, file_name, train)
 
     return ContinualScenario(encoded_dataset)
-
-
