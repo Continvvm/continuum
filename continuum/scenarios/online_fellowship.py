d40b0aabef794339e0d3767c1f40e6f1b935907e
TLESORT <t.lesort@gmail.com>
2021-10-14 14:41:28 -0400

    add update labels possibility

--- continuum/scenarios/online_fellowship.py
+++ continuum/scenarios/online_fellowship.py
@@ -1,5 +1,5 @@
 from typing import Callable, List, Union
-
+from torchvision import transforms
 import numpy as np
 
 from continuum.datasets import _ContinuumDataset
@@ -24,6 +24,7 @@ class OnlineFellowship(_BaseScenario):
     ) -> None:
         self._nb_tasks = len(cl_datasets)
         self.cl_datasets = cl_datasets
+        self.update_labels = update_labels
         # init with first task
         super().__init__(cl_dataset=cl_datasets[0], nb_tasks=1, transformations=transformations)
 
@@ -31,10 +32,37 @@ class OnlineFellowship(_BaseScenario):
             # if we have a list of transformations, it should be a transformation per cl_dataset
             assert len(transformations) == self._nb_tasks
 
+        _tot_num_classes = 0
+        label_trsf = []
+        self.list_classes = []
+        for task_ind, cl_dataset in enumerate(cl_datasets):
+            if self.update_labels:
+                # we just shift the label number by the nb of classes seen so far
+                label_trsf.append(transforms.Lambda(lambda x: x + _tot_num_classes))
+                self.list_classes += (list(np.array(cl_dataset.classes)+_tot_num_classes))
+            else:
+                self.list_classes += cl_dataset.classes
+            _tot_num_classes += cl_dataset.nb_classes
+
+        if not self.update_labels:
+            label_trsf = None
+        self.target_trsf = label_trsf
+
+
     @property
     def nb_tasks(self) -> int:
         """Number of tasks in the whole continual setting."""
         return self._nb_tasks
+
+    @property
+    def nb_classes(self) -> int:
+        """Number of tasks in the whole continual setting."""
+        return len(self.classes)
+
+    @property
+    def classes(self) -> List:
+        """Number of tasks in the whole continual setting."""
+        return np.unique(self.list_classes)
 
     def __getitem__(self, task_index: Union[int, slice]):
         """Returns a task by its unique index.
@@ -55,6 +83,7 @@ class OnlineFellowship(_BaseScenario):
         return TaskSet(
             x, y, t,
             trsf=self.trsf[task_index] if isinstance(self.trsf, list) else self.trsf,
+            target_trsf=self.target_trsf[task_index] if isinstance(self.target_trsf, list) else self.target_trsf,
             data_type=self.cl_dataset.data_type,
             bounding_boxes=self.cl_dataset.bounding_boxes
         )
