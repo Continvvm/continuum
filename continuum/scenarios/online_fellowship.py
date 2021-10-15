2a407cf0738426c0a94618abd69f2adc9025b8d8
TLESORT <t.lesort@gmail.com>
2021-10-15 10:09:57 -0400

    convert into online evaluation of class number

--- continuum/scenarios/online_fellowship.py
+++ continuum/scenarios/online_fellowship.py
@@ -8,7 +8,8 @@ from continuum.tasks import TaskSet
 
 
 class OnlineFellowship(_BaseScenario):
-    """A scenario to create large fellowship and load them one by one. No fancy stream, one cl_dataset = one task
+    """A scenario to create large fellowship and load them one by one. No fancy stream, one cl_dataset = one task.
+    The advantage toward using a Fellowship dataset is that the datasets might have different data_type.
 
     :param cl_datasets: A list of continual dataset.
     :param transformations: A list of transformations applied to all tasks. If
@@ -23,44 +24,31 @@ class OnlineFellowship(_BaseScenario):
             cl_datasets: List[_ContinuumDataset],
             transformations: Union[List[Callable], List[List[Callable]]] = None,
             update_labels=True,
-            list_dict_args=[{"data_path":".", "train": True, "download": False}]
+            list_dict_args=[{"data_path": ".", "train": True, "download": False}]
     ) -> None:
         self.cl_datasets = cl_datasets
         self.update_labels = update_labels
         self.list_dict_args = list_dict_args
-        assert len(self.list_dict_args)==1 or len(self.list_dict_args)==len(cl_datasets)
+        self.transformations = transformations
+        assert len(self.list_dict_args) == 1 or len(self.list_dict_args) == len(cl_datasets)
         # init with first task
         if isinstance(cl_datasets[0], InMemoryDataset):
             self.cl_dataset = cl_datasets[0]
         else:
             self.cl_dataset = cl_datasets[0](**self._get_args(0))
-        super().__init__(cl_dataset=self.cl_dataset, nb_tasks=1, transformations=transformations)
-        self._nb_tasks = len(cl_datasets)
 
         if isinstance(self.trsf, list):
             # if we have a list of transformations, it should be a transformation per cl_dataset
             assert len(transformations) == self._nb_tasks
 
-        # We count total number of classes here and define label_trsf if necessary if (update_labels==True)
-        _tot_num_classes = 0
-        label_trsf = []
-        self.list_classes = []
-        for task_ind, cl_dataset in enumerate(cl_datasets):
-            if isinstance(cl_dataset, InMemoryDataset):
-                dataset = cl_dataset
-            else:
-                dataset = cl_dataset(**self._get_args(task_ind))
-            if self.update_labels:
-                # we just shift the label number by the nb of classes seen so far
-                label_trsf.append(transforms.Lambda(lambda x: x + _tot_num_classes))
-                self.list_classes += (list(np.array(dataset.classes)+_tot_num_classes))
-            else:
-                self.list_classes += dataset.classes
-            _tot_num_classes += dataset.nb_classes
+        trsf_0 = self.get_trsf(task_index=0)
 
-        if not self.update_labels:
-            label_trsf = None
-        self.target_trsf = label_trsf
+        self.label_trsf = []
+        self.list_classes_sofar = []
+        self.list_task_sofar = []
+
+        super().__init__(cl_dataset=self.cl_dataset, nb_tasks=1, transformations=trsf_0)
+        self._nb_tasks = len(cl_datasets)
 
     def _get_args(self, ind_task):
         if len(self.list_dict_args) == 1:
@@ -68,6 +56,20 @@ class OnlineFellowship(_BaseScenario):
         else:
             return self.list_dict_args[ind_task]
 
+    def _get_trsf(self, ind_task):
+        if isinstance(self.transformations, list):
+            trsf = self.transformations[ind_task]
+        else:
+            trsf = self.transformations
+        return trsf
+
+    def _get_label_trsf(self, ind_task):
+        label_trsf = None
+        if self.update_labels:
+            if not (ind_task in self.list_task_sofar):
+                raise AssertionError("The future can not be indexed, task should be visited in the right order.")
+            label_trsf = self.label_trsf[ind_task]
+        return label_trsf
 
     @property
     def nb_tasks(self) -> int:
@@ -82,7 +84,8 @@ class OnlineFellowship(_BaseScenario):
     @property
     def classes(self) -> List:
         """List of classes in the whole continual setting."""
-        return np.unique(self.list_classes)
+        print("classes seen so far. You can not know the total number of class before visiting all tasks.")
+        return np.unique(self.list_classes_sofar)
 
     def __getitem__(self, task_index: Union[int, slice]):
         """Returns a task by its unique index.
@@ -94,22 +97,34 @@ class OnlineFellowship(_BaseScenario):
         """
         if isinstance(task_index, slice):
             raise NotImplementedError(
-                f"You cannot select multiple task ({task_index}) on OnlineFellowship yet"
+                f"You cannot select multiple task ({task_index}) on OnlineFellowship scenario yet"
             )
+
+        if self.update_labels:
+            # we just shift the label number by the nb of classes seen so far
+            self.label_trsf.append(transforms.Lambda(lambda x: x + len(self.list_classes_sofar)))
+
+        label_trsf = self._get_label_trsf(task_index)
+
         if isinstance(self.cl_datasets[task_index], InMemoryDataset):
             self.cl_dataset = self.cl_datasets[task_index]
         else:
             self.cl_dataset = self.cl_datasets[task_index](**self._get_args(task_index))
+
         x, y, _ = self.cl_dataset.get_data()
+        self.list_classes_sofar += np.unique(y)
+        self.list_task_sofar.append(task_index)
+
         t = np.ones(len(y)) * task_index
 
         return TaskSet(
             x, y, t,
-            trsf=self.trsf[task_index] if isinstance(self.trsf, list) else self.trsf,
-            target_trsf=self.target_trsf[task_index] if isinstance(self.target_trsf, list) else self.target_trsf,
+            trsf=self._get_trsf(task_index),
+            target_trsf=label_trsf,
             data_type=self.cl_dataset.data_type,
             bounding_boxes=self.cl_dataset.bounding_boxes
         )
 
-    def _setup(self, nb_task):
-        return nb_task
+
+def _setup(self, nb_task):
+    return nb_task
