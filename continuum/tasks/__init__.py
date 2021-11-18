5e761d52d1f5e9ba0a91b944099f270f2a557060
TLESORT <t.lesort@gmail.com>
2021-11-17 20:31:39 -0500

    add BaseTaskSet to init to be able to check taskset type

--- continuum/tasks/__init__.py
+++ continuum/tasks/__init__.py
@@ -1,7 +1,7 @@
 # pylint: disable=C0401
 # flake8: noqa
 from continuum.tasks.task_set import TaskSet
-from continuum.tasks.base import TaskType
+from continuum.tasks.base import BaseTaskSet, TaskType
 from continuum.tasks.utils import split_train_val, concat, get_balanced_sampler
 
 __all__ = ["TaskSet", "TaskType"]
