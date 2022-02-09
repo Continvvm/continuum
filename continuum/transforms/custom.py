07de72824c61e13406b6add8023406fb2d056ffe
Lucas Cecchi <lucascecchi@gmail.com>
2022-02-09 18:51:21 -0500

    Add support and test for tensor input to BackgroundSwap

--- continuum/transforms/custom.py
+++ continuum/transforms/custom.py
@@ -1,40 +1,95 @@
-from continuum.datasets import _ContinuumDataset
-from typing import Tuple
+from typing import Tuple, Union
 import numpy as np
+import torch
+from torch.utils.data import DataLoader
+from continuum.datasets import _ContinuumDataset
 
 
 class BackgroundSwap:
+    """
+    Swap input image background with a randomly selected image from bg_images dataset
 
-    def __init__(self, bg_images: _ContinuumDataset, input_dim: Tuple[int, int] = (28, 28)) -> None:
-        """
-        :param bg_images: background image set.
-        :param input_dim: input dimension of transform, excluding channels
-        """
+    :param bg_images: background image dataset, must be normalized.
+    :param input_dim: input dimension of transform, excluding channels
+    :param normalize_input: normalize input images, if false it's assumed they are already normalized
+    :param normalize_bg: normalize bg images, if false it's assumed they are already normalized
+    :param crop_bg: crop background images to correct size, if false it's assumed they are cropped
+    """
+
+    def __init__(self, bg_images: _ContinuumDataset, input_dim: Tuple[int, int] = (28, 28),
+                 normalize_input: bool = True,
+                 normalize_bg: bool = True,
+                 crop_bg: bool = True):
+        self.normalize_input = normalize_input
+        self.normalize_bg = normalize_bg
+        self.crop_bg = crop_bg
         self.input_dim = input_dim
         self.bg_images = bg_images.get_data()[0]
 
-    def _randcrop(self, img: np.ndarray) -> np.ndarray:
+    def _randcrop(self, img: Union[np.ndarray, torch.Tensor]) -> Union[np.ndarray, torch.Tensor]:
+        """
+        Crop input image to self.input_dim shape
+        :param img: input image
+        """
+
+        crop_height = img.shape[0] >= self.input_dim[0]
+        crop_width = img.shape[1] >= self.input_dim[1]
+
         x_crop = np.random.randint(0, img.shape[0] - self.input_dim[0])
         y_crop = np.random.randint(0, img.shape[1] - self.input_dim[1])
 
-        return img[x_crop:x_crop + self.input_dim[0], y_crop: y_crop + self.input_dim[1], :]
+        if crop_width and crop_height:
+            return img[x_crop:x_crop + self.input_dim[0], y_crop: y_crop + self.input_dim[1], :]
 
-    def __call__(self, img: np.ndarray, mask: bool = None) -> np.ndarray:
+        elif crop_width:
+            return img[:, y_crop: y_crop + self.input_dim[1], :]
+
+        elif crop_height:
+            return img[x_crop:x_crop + self.input_dim[0], :, :]
+
+        else:
+            raise Exception("Background image is smaller than foreground image")
+
+    def __call__(self, img: Union[np.ndarray, torch.Tensor],
+                 mask: Union[np.ndarray, torch.BoolTensor] = None) -> Union[np.ndarray, torch.Tensor]:
         """
-        :param img: input image
+        Splice input image foreground with randomly sampled background
+
+        NOTE: inputting a torch.Tensor assumes the channel dim comes first,
+        while inputting a np.ndarray requires the channel dim to come second
+
+        :param img: input image, must be normalized
         :param mask: boolean mask for the foreground of img, if None then a .5 threshold is used
         """
 
-        # TODO: don't assume img is 2 dimensional (h, w)
+        if isinstance(img, torch.Tensor):
+            # img.unsqueeze_(0)
+            img = img.repeat(3, 1, 1)
+            img = img.permute(1, 2, 0)
 
-        img = np.expand_dims(img, 2)
-        img = img / 255.0
-        img = np.concatenate([img, img, img], axis=2)
+        else:
+            img = np.expand_dims(img, 2)
+            img = np.concatenate([img, img, img], axis=2)
+
+        new_background = self.bg_images[np.random.randint(0, len(self.bg_images))]
+
+        if self.normalize_input:
+            img = img / 255.0
+
+        if self.normalize_bg:
+            new_background = new_background / 255.0
+
+        if self.crop_bg:
+            new_background = self._randcrop(new_background)
 
         if mask is None:
             mask = (img > .5)
 
-        bg = self.bg_images[np.random.randint(0, len(self.bg_images))] / 255.0
-        bg = self._randcrop(bg)
+        out = mask * img + ~mask * new_background
 
-        return mask * img + np.ma.logical_not(mask) * bg
+        if isinstance(out, torch.Tensor):
+            out = out.permute(2, 0, 1)
+
+        return out
+
+
--- tests/test_background_swap.py
+++ tests/test_background_swap.py
@@ -1,10 +1,14 @@
 from continuum.transforms.custom import BackgroundSwap
 from continuum.datasets import CIFAR10
 from continuum.datasets import MNIST
+import torchvision
+from continuum.scenarios import TransformationIncremental
 from matplotlib import pyplot as plt
+import pytest
 
 
-def test_background_swap():
+@pytest.mark.slow
+def test_background_swap_numpy():
     mnist = MNIST("MNIST_DATA", download=True, train=True)
     cifar = CIFAR10("CIFAR10_DATA", download=True, train=True)
 
@@ -12,7 +16,25 @@ def test_background_swap():
 
     im = mnist.get_data()[0][0]
     im = bg_swap(im)
-    plt.imshow(im, interpolation='nearest')
-    plt.show()
 
 
+@pytest.mark.slow
+def test_background_swap_torch():
+    cifar = CIFAR10("CIFAR10_DATA", download=True, train=True)
+
+    mnist = torchvision.datasets.MNIST('./TorchMnist/', train=True, download=True,
+                                       transform=torchvision.transforms.Compose([
+                                           torchvision.transforms.ToTensor(),
+                                           torchvision.transforms.Normalize(
+                                               (0.5,), (0.5,))
+                                       ]))
+
+    bg_swap = BackgroundSwap(cifar, input_dim=(28, 28), normalize_input=False)
+    im = mnist[0][0]
+
+    im = bg_swap(im)
+
+
+
+def test_transform_incremental_bg_swap():
+    pass
